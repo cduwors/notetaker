e43b02cc6264e1a8121a37be9987b7131dca98fc
cduwors <christinaduwors@gmail.com>
2022-08-14 14:47:27 -0400

    server set up on heroku

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const PORT = process.env.PORT || 3001;
 const app = express();
 const fs = require("fs");
 
@@ -21,6 +22,6 @@ app.get("/api/notes", (req, res) => {
 	res.json(results);
 });
 
-app.listen(3001, () => {
-	console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+	console.log(`API server now on port ${PORT}!`);
 });
