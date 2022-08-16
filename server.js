3b477c0363fe26a314d02b300a8d8839c118f9c7
cduwors <christinaduwors@gmail.com>
2022-08-16 11:04:28 -0400

    fixed router const

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const fs = require("fs");
 
-const apiRoutes - require("./routes/apiRoutes.js")
-const htmlRoutes - require("./routes/htmlRoutes.js")
-
+const apiRoutes = require("./routes/apiRoutes.js");
+const htmlRoutes = require("./routes/htmlRoutes.js");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
